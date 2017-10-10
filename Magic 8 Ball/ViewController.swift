2c605167ac8626021e81bcbac18878b2fbbf4d26
Sain-R Edwards Jr <SKE@sain-rs-imac-5.local>
2017-10-09 21:40:59 -0700

    1. Created IBOutlet for the Image View and IBAction for the button. 2. Added code in the View Controller to allow the app to function.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var eightBallImageView: UIImageView!
+    
+    var eightBallQuestionIndex: Int = 0
+    
+    var ballNames: [String] = ["ball1", "ball2", "ball3", "ball4", "ball5"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateEightBallAnswer()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func askButtonTapped(_ sender: UIButton) {
+        updateEightBallAnswer()
     }
-
-
+    
+    func updateEightBallAnswer() {
+        eightBallQuestionIndex = Int(arc4random_uniform(5))
+        eightBallImageView.image = UIImage(named: ballNames[eightBallQuestionIndex])
+    }
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateEightBallAnswer()
+    }
 }
 
